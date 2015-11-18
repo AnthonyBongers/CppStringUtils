3140ccafac6d3b18bbc2426cc95e257097b4aae2
Anthony Bongers <bongers.anthony@gmail.com>
2015-11-18 14:21:39 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,18 @@
 #include <sstream>
 using namespace std;
 
+// End-case: There are no more parameters to output.
+// Occurs when no parameters are given to the join function.
 void join_impl(stringstream &stream, string separator) {}
 
+// End-case: The last argument to output. End the recursion here.
 template <typename Arg>
 void join_impl(stringstream &stream, string separator, Arg last) 
 {
 	stream << last;
 }
 
+// Recursively output the arguments to the stream until an end-case is hit.
 template <typename Arg, typename ...Rest>
 void join_impl(stringstream &stream, string separator, Arg first, Rest ...rest)
 {
@@ -29,5 +33,12 @@ string join(string separator, Args ...args)
 
 int main() 
 {
+	cout << join(", ", "test", 1, 1.32) << endl;
+	// Output: "test, 1, 1.32" 
+	
+	cout << join(", ", "test") << endl;
+	// Output: "test"
+	
 	cout << join(", ") << endl;
+	// Outputs an empty string.
 }
