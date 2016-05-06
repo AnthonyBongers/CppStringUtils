e12ad0fa81f79f43218c96f062b03afe55704941
Anthony Bongers <bongers.anthony@gmail.com>
2016-05-06 10:17:03 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,45 +1,20 @@
 #include <iostream>
-#include <sstream>
-using namespace std;
 
-// End-case: There are no more parameters to output.
-// Occurs when no parameters are given to the join function.
-void join_impl(stringstream &stream, string separator) {}
+#include "Join.h"
+#include "Concat.h"
+#include "Split.h"
 
-// End-case: The last argument to output. End the recursion here.
-template <typename Arg>
-void join_impl(stringstream &stream, string separator, Arg last) 
+using namespace string_utils;
+
+int main()
 {
-	stream << last;
-}
-
-// Recursively output the arguments to the stream until an end-case is hit.
-template <typename Arg, typename ...Rest>
-void join_impl(stringstream &stream, string separator, Arg first, Rest ...rest)
-{
-	stream << first << separator;
-	join_impl(stream, move(separator), rest...);
-}
-
-// Join data together with a custom separator. 
-template <typename ...Args>
-string join(string separator, Args ...args)
-{
-	stringstream stream;
-	
-	join_impl(stream, move(separator), args...);
-	
-	return stream.str();
-}
-
-int main() 
-{
-	cout << join(", ", "test", 1, 1.32) << endl;
-	// Output: "test, 1, 1.32" 
-	
-	cout << join(", ", "test") << endl;
-	// Output: "test"
-	
-	cout << join(", ") << endl;
-	// Outputs an empty string.
+  std::cout << join(", ", "Anthony", 2, 934.23, 'H') << std::endl;
+  // Output: Anthony, 2, 934.23, H
+  
+  std::cout << concat("Anthony ", std::boolalpha, false, ' ', 23) << std::endl;
+  // Output: Anthony false 23
+  
+  auto words = split("My name is Anthony Bongers", " ");
+  std::cout << "The sentence contains " << words.size() << " words." << std::endl;
+  // Output: The sentence contains 5 words.
 }
